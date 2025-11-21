b8ae8208a56100f87a67cb6c43a5ead9cb61534c
lipvxz <filipematos1821@gmail.com>
2025-11-20 21:51:46 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sonhar-colchoes/sw.js', { scope: '/sonhar-colchoes/' })})}
